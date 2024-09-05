802c6feca22548dbadf93df6060a85c6f3d4e8e1
jyoon66 <asian.tiger.juice@gmail.com>
2024-09-04 23:06:57 -0700

    Add bonus1.py and bug_fixing_exercises.py

---
+++ bug_fixing_exercises.py
@@ -0,0 +1,24 @@
+# Exercise 1
+# answers = ['Yes', 'No', 'Yes', 'No', 'No'
+answers = ['Yes', 'No', 'Yes', 'No', 'No']  # closed ]
+
+# Exercise 2
+# my_answer = input("What is your answer?")
+# answers = ['Yes', 'No', 'Yes' 'No' my_answer]
+
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes' 'No', my_answer] # added a comma before last element
+
+# Excercise 3
+# my_answer = input(What is your answer?)
+# answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+my_answer = input("What is your answer?")  # added quotations
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+# Exercise 4
+# my_answer = input["What is your answer?"]
+# answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+my_answer = input("What is your answer?")  # Changed to parentheses
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
